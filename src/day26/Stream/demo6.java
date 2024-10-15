4eec878a7781aa3726088a4abed31088144d6d24
wangshimin <shimin.wang@foxmail.com>
2024-10-15 16:01:39 +0800

    Stream流 - Stream流的中间方法：filter、limit、skip、distinct、concat、map

---
+++ src/day26/Stream/demo6.java
@@ -0,0 +1,65 @@
+package day26.Stream;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.function.Predicate;
+
+/**
+ * filter 过滤
+ */
+public class demo6 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, "张无忌", "周芷若", "赵敏", "张强", "张三丰", "张翠山","张亮","谢昆");
+
+        System.out.println("--------- filter 过滤 ---------------");
+        // 把"张"开头的留下，其余的数据过滤不要
+
+        /*
+        list.stream().filter(new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                // true  当前数据留下
+                // false 当前数据舍弃不要
+                return s.startsWith("张");
+            }
+        }).forEach(s-> System.out.println(s));
+        */
+
+        // 改成Lambda表达式
+        list.stream()
+                .filter(s -> s.startsWith("张"))
+                .filter(s -> s.length() == 3)
+                .forEach(s -> System.out.println(s));
+
+        System.out.println("---------!! 修改Stream流中的数据，不会影响原来集合或数组中的数据 !! -------------");
+        System.out.println(list);
+
+        System.out.println("--------- limit 获取前几个数据 ---------------");
+        list.stream()
+                .limit(3).
+                forEach(s -> System.out.println(s));
+
+        System.out.println("--------- skip  跳过前几个数据 ---------------");
+        list.stream()
+                .skip(3)
+                .forEach(s -> System.out.println(s));
+
+        System.out.println("--------- 课堂练习 ---------------");
+        // 只显示第4-6个数据 张强 张三丰 张翠山
+        System.out.println("--------- 思路1 ---------------");
+        // 跳过3个，再只显示3个
+        list.stream()
+                .skip(3)
+                .limit(3)
+                .forEach(s -> System.out.println(s));
+        System.out.println("--------- 思路2 ---------------");
+        // 显示前6个，再跳过3个
+        list.stream()
+                .limit(6)
+                .skip(3)
+                .forEach(s -> System.out.println(s));
+
+    }
+}
--- src/day26/Stream/note.md
+++ src/day26/Stream/note.md
@@ -2,15 +2,31 @@
 #### 作用
  结合了Lambda表达式，简化集合、数组的操作
 
-| 获取方式 | 方法 | 说明  |
-|-----|----|---|
-| 单列集合 | default Stream<E> stream()|  获取集合的Stream流，Collection接口中的默认方法 |
-| 双列集合 | 无  |无法直接使用Stream流|
-| 数组  | public static <T> Stream<T> stream(T[] array) |获取数组的Stream流，Arrays工具中的静态方法|
-| 一堆散列数据 | public static Stream<T> of(T... values) | Stream接口中的静态方法|
-
 #### 使用步骤
 1. 先创建一个Stream流（流水线），并把数据放上去
+
+   | 获取方式 | 方法                                                | 说明 |
+   |------|---------------------------------------------------|----|
+   | 单列集合 | default Stream\<E\> stream()                      | 获取集合的Stream流，Collection接口中的默认方法   |
+   | 双列集合 | 无                                                 | 无法直接使用Stream流  |
+   | 数组   | public static \<T\> Stream\<T\> stream(T[] array) | 获取数组的Stream流，Arrays工具中的静态方法   |
+   | 一堆散列数据 | public static Stream\<T\> of(T... values)                                                  | Stream接口中的静态方法   |
+
+
 2. 利用流中的API进行各种操作
     - 2.1 使用**中间方法**对流水线上的数据进行操作
-    - 2.2 使用**终结方法**对流水线上的数据进行操作
+   
+      | 名称                                                      | 说明                             |
+      |---------------------------------------------------------|--------------------------------|
+      | Stream\<T\> **filter**(Predicate<? super T> predicate)  | 过滤                             |
+      | Stream\<T\> **limit**(long maxSize)                     | 获取前几个元素                        |
+      | Stream\<T\> **skip**(long n)                            | 跳过前几个元素                        |                                                    |         |
+      | Stream\<T\> **distinct**()                              | 元素去重，依赖（集合中的hashCode和equals方法） |
+      | static \<T\> Stream\<T\> **concat**(Stream a, Stream b) | 合并两个流                          |
+      | Stream\<R\> **map**(Function<T, R> mapper)              | 转换流中的数据类型                      |
+
+      > 注意！！
+      > - 中间方法，返回新的Stream流，原来的Stream流只能使用一次，建议使用链式编程
+      > - 修改Sream流中的数据，不会影响原来的数据
+
+  - 2.2 使用**终结方法**对流水线上的数据进行操作
